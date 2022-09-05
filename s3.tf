67acc2e78fbd442e5b4bdee652418b0d54a115e9
Ryan Archibald <ryan.archibald@and.digital>
2022-09-05 12:23:19 +0100

    adding s3 bucket and variables for the bucket

---
+++ s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "and-demo" {
+  bucket = var.s3_bucket_name
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "aws_region" {
   description = "The region that AWS resources will be used in"
   type        = string
 }
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  type = string
+  default = "and-cloud-demo"
+}
+
+variable "tags" {
+  description = "Tags to set on the s3 bucket"
+  type = map(string)
+  default = {
+    "Owner" = "AND-Cloud"
+    "Environment" = "Dev"
+  }
+}
