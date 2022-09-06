ba2253f12d659be730703a5c4d62b7e7a51c5866
Ryan Archibald <ryan.archibald@and.digital>
2022-09-06 14:40:24 +0100

    formatting the code

--- variables.tf
+++ variables.tf
@@ -5,15 +5,15 @@ variable "aws_region" {
 
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket"
-  type = string
-  default = "and-cloud-demo"
+  type        = string
+  default     = "and-cloud-demo"
 }
 
 variable "tags" {
   description = "Tags to set on the s3 bucket"
-  type = map(string)
+  type        = map(string)
   default = {
-    "Owner" = "AND-Cloud"
+    "Owner"       = "AND-Cloud"
     "Environment" = "Dev"
   }
 }
